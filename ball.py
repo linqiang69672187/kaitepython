4eeedd6e8230803dc907b1325128533905a3d5ad
林强 <69672187@qq.com>
2018-11-13 19:13:41 +0800

    碰撞精灵

--- ball.py
+++ ball.py
@@ -18,29 +18,38 @@ class MyBallClass(pygame.sprite.Sprite):
         if self.rect.top < 0 or self.rect.bottom > height:
             self.speed[1] = -self.speed[1]
 
+def animate(group):
+    screen.fill([255,255,255])
+    for ball in group:
+        group.remove(ball)
+        if pygame.sprite.spritecollide(ball,group,False):
+            ball.speed[0] = -ball.speed[0]
+            ball.speed[1] = -ball.speed[1]
+        group.add(ball)
+        ball.move()
+        screen.blit(ball.image,ball.rect)
+    pygame.display.flip()
+    pygame.time.delay(20)
+
 
 size = width,height=640,480
 screen = pygame.display.set_mode(size)
 screen.fill([255,255,255])
 img_file="Orb_Icons_001.png"
-balls=[]
+group = pygame.sprite.Group()
 for row in range(0,3):
     for column in range(0,3):
         location=[column*180+10,row*180+10]
         speed = [choice([-2,2]),choice([-2,2])]
         ball=MyBallClass(img_file,location,speed)
-        balls.append(ball)
+        group.add(ball)
 #ceshi11
 running=True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    pygame.time.delay(20)
-    screen.fill([255, 255, 255])
-    for ball in balls:
-        ball.move()
-        screen.blit(ball.image, ball.rect)
-    pygame.display.flip()
+
+    animate(group)
 
 pygame.quit()
