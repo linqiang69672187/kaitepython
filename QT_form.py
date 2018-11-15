9c92dafe6ad730d9716a90ffd003eb6c6b081b16
linqiang69672187 <69672187@qq.com>
2018-11-15 17:05:44 +0800

    QTGUI

--- QT_form.py
+++ QT_form.py
@@ -7,6 +7,15 @@ class MyPyQT_Form(QtWidgets.QMainWindow,form_class):
     def __init__(self,parent=None):
         QtWidgets.QMainWindow.__init__(self,parent)
         self.setupUi(self)
+        self.pushButton.clicked.connect(self.button_clicked)
+
+    def button_clicked(self):
+        x = self.pushButton.x();
+        y = self.pushButton.y();
+        x+=5
+        y+=5
+        self.pushButton.move(x,y)
+        self.textEdit.append(str(x)+str(y))
 
 app = QtWidgets.QApplication(sys.argv)
 myWindow = MyPyQT_Form()
