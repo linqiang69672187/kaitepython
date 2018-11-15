d6474ffa783feb80fa5acd634882044a15ae8c6c
linqiang69672187 <69672187@qq.com>
2018-11-15 09:27:31 +0800

    乒乓球三条命

--- pypony.py
+++ pypony.py
@@ -12,12 +12,15 @@ class MyBallClass(pygame.sprite.Sprite):
         self.speed = speed
 
     def move(self):
+        global score_font,score,score_surf
         self.rect = self.rect.move(self.speed)
         if self.rect.left < 0 or self.rect.right > screen.get_width():
             self.speed[0]=-self.speed[0]
 
         if self.rect.top<0:
             self.speed[1]=-self.speed[1]
+            score = score  + 1
+            score_surf = score_font.render(str(score),1,(0,0,0))
 class MyPaddleClass(pygame.sprite.Sprite):
     def __init__(self,location = [0,0]):
         pygame.sprite.Sprite.__init__(self)
@@ -34,7 +37,15 @@ ball_speed=[10,5]
 myBall = MyBallClass("pinpang.png",ball_speed,[50,50])
 ballGroup = pygame.sprite.Group(myBall)
 paddle = MyPaddleClass([270,400])
+lives = 3
+score = 0
+score_font = pygame.font.Font(None,50)
+score_surf = score_font.render(str(score),1,(0,0,0))
+score_pos = [10,10]
+
 pygame.mouse.set_visible(False)
+
+done = False
 running = True
 while running:
     clock.tick(30)
@@ -48,7 +59,30 @@ while running:
     if pygame.sprite.spritecollide(paddle,ballGroup,False):
         myBall.speed[1] = -myBall.speed[1]
     myBall.move()
-    screen.blit(myBall.image,myBall.rect)
-    screen.blit(paddle.image,paddle.rect)
-    pygame.display.flip()
+    if not done:
+        screen.blit(myBall.image, myBall.rect)
+        screen.blit(paddle.image, paddle.rect)
+        screen.blit(score_surf, score_pos)
+        for i in range(lives):
+            width = screen.get_width()
+            screen.blit(myBall.image, [width - 40 * i, 20])
+        pygame.display.flip()
+
+    if myBall.rect.top >= screen.get_rect().bottom:
+        lives = lives -1
+        if lives  == 0:
+            final_text1 = "游戏结束"
+            final_text2 = "你的最后得分:"+str(score)
+            ft1_font = pygame.font.Font('Fonts/Muyao-Softbrush.ttf',64)
+            ft1_surf = ft1_font.render(final_text1,1,(0,0,0))
+            ft2_font = pygame.font.Font('Fonts/Muyao-Softbrush.ttf',44)
+            ft2_surf = ft1_font.render(final_text2, 1, (0, 0, 0))
+            screen.blit(ft1_surf,[screen.get_width()/2-ft1_surf.get_width()/2,100])
+            screen.blit(ft2_surf,[screen.get_width()/2-ft2_surf.get_width()/2,200])
+            pygame.display.flip()
+            done = True
+        else:
+            pygame.time.delay(2000)
+            myBall.rect.topleft = [50,50]
+
 pygame.quit()
