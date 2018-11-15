68f6fb0fd96117fde63f80024f0e1d0e8f2b92f4
linqiang69672187 <69672187@qq.com>
2018-11-15 15:58:49 +0800

    QTGUI

--- QT_form.py
+++ QT_form.py
@@ -1,24 +1,14 @@
 import sys
-from PyQt5 import QtWidgets
+from PyQt5 import QtCore,QtGui,uic,QtWidgets
 from untitled import Ui_Dialog
 
-class MyPyQT_Form(Ui_Dialog):
-    def __init__(self):
-        super(MyPyQT_Form,self).__init__()
+form_class = uic.loadUiType("myfirstgui.ui")[0]
+class MyPyQT_Form(QtWidgets.QMainWindow,form_class):
+    def __init__(self,parent=None):
+        QtWidgets.QMainWindow.__init__(self,parent)
         self.setupUi(self)
 
-
-
-    def initUI(self):
-        self.setWindowTitle("QFileDialog")
-        self.setGeometry(400,400,300,260)
-
-
-        self.setLayout(self.mainLayout)
-
-
-if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
-    my_pyqt_form = MyPyQT_Form()
-    my_pyqt_form.show()
-    sys.exit(app.exec_())
+app = QtWidgets.QApplication(sys.argv)
+myWindow = MyPyQT_Form()
+myWindow.show()
+app.exec_()
